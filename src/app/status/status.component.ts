c7b15fb462dfcc947fb43edfc8c3f4f5cda55067
rlacey2 <rlacey2@gmail.com>
2025-11-09 00:38:56 +0000

    status scrolling2

--- src/app/status/status.component.ts
+++ src/app/status/status.component.ts
@@ -15,7 +15,7 @@ import {CdkScrollable} from '@angular/cdk/scrolling';
 @Component({
   selector: 'status',
   templateUrl: 'status.component.html',
-  styleUrls: ['status.component.scss'],
+  styleUrls: [ 'status.component.scss'],
   imports: [MatSidenavContainer, MatSidenav, MatSidenavContent, RouterLink]
 })
 export class StatusComponent {
